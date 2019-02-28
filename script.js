b0ee7ced8f68e6044057cd7fe11929decbabe643
Mohit Kewalramani <mohitkewalramani74@gmail.com>
2019-02-27 19:28:08 -0800

    First draft of app implementation

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function loginButtonClick(){
+    window.location.replace("project_list.html");
+}
+
+function logOut(){
+    window.location.replace("login.html");
+}
+
+function navigateToProjectPage(){
+    window.location.replace("project_main.html")
+}
